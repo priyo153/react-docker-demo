93654ebd16b82016d64ae73f6559abe1d4e129bf
priyojit <priyojit@etranssolutions.com>
2021-08-29 10:54:03 +0530

    ui chng

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          DEMO priyojit 123 <code>src/App.js</code> and save to reload.
-        </p>
+        <p>demo project by priyojit</p>
         <a
           className="App-link"
           href="https://reactjs.org"
